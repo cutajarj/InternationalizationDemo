4dcf701b5f7ff894661f25f1eb2358472a4a1a42
cutajarj <james.cutajar@gmail.com>
2021-05-13 08:50:09 +0100

    Initial commit

---
+++ src/main/java/com/cutajarjames/internationalizationdemo/Wiring.java
@@ -0,0 +1,19 @@
+package com.cutajarjames.internationalizationdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class Wiring {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+}
---
+++ src/test/java/com/cutajarjames/internationalizationdemo/InternationalizationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutajarjames.internationalizationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalizationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
