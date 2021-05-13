d03a66c3c68d1038aed64c75abbaf864cd8704d0
cutajarj <james.cutajar@gmail.com>
2021-05-13 08:50:09 +0100

    Initial commit

---
+++ src/main/java/com/cutajarjames/internationalizationdemo/InternationalizationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cutajarjames.internationalizationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalizationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternationalizationDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cutajarjames/internationalizationdemo/controllers/HelloController.java
@@ -0,0 +1,31 @@
+package com.cutajarjames.internationalizationdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        String message = "Hello AWS Elastic Beanstalk!";
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            message += " From host: " + ip;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "mainPage";
+    }
+}
